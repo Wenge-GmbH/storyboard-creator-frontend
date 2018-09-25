836e1bee00ff055c73d6c5341e0adbb9b6a322e9
Markus Maelzer <markus.maelzer@gmail.com>
2018-09-25 11:41:11 +0200

    added formating plugin with Slate "Marks"

--- src/components/slate/index.js
+++ src/components/slate/index.js
@@ -44,20 +44,40 @@ export default class SlateEditor extends Component {
 
   onKeyDown = (e, change) => {
     writeOutAnd(e, change);
-    if (e.key != '+' || !e.ctrlKey) return;
-    e.preventDefault();
-    // Determine whether any of the currently selected blocks are code blocks.
-    const isCode = change.value.blocks.some(block => block.type == 'code')
-    change.setBlocks(isCode ? 'paragraph' : 'code');
-    return true;
+    if (!e.ctrlKey) return;
+    switch (e.key) {
+      // When "`" is pressed, keep our existing code block logic.
+      case '+': {
+        e.preventDefault();
+        // Determine whether any of the currently selected blocks are code blocks.
+        const isCode = change.value.blocks.some(block => block.type === 'code')
+        change.setBlocks(isCode ? 'paragraph' : 'code');
+        return true;
+      }
+      default:
+        return;
+    }
   }
 
   renderNode = props => {
     switch (props.node.type) {
       case 'code':
         return <CodeNode {...props} />;
+    }
+  }
 
-
+  renderMark = props => {
+    switch (props.mark.type) {
+      case 'bold':
+        return <strong>{props.children}</strong>
+      case 'code':
+        return <code>{props.children}</code>
+      case 'italic':
+        return <em>{props.children}</em>
+      case 'strikethrough':
+        return <del>{props.children}</del>
+      case 'underline':
+        return <u>{props.children}</u>
     }
   }
 
@@ -65,11 +85,12 @@ export default class SlateEditor extends Component {
     return (
       <div className="editor">
         <Editor
+          plugins={plugins}
           value={this.state.value}
           onChange={this.onChange}
           onKeyDown={this.onKeyDown}
           renderNode={this.renderNode}
-
+          renderMark={this.renderMark}
         />
       </div>
     )
@@ -82,3 +103,24 @@ const CodeNode = ({ attributes, children }) => (
     <code>{children}</code>
   </pre>
 )
+
+// const BoldMark= ({ children }) => <strong>{children}</strong>;
+
+const MarkHotkey = ({type, key}) => ({
+    onKeyDown(e, change) {
+      if (!e.ctrlKey || e.key !== key) return;
+      // Prevent the default characters from being inserted.
+      e.preventDefault()
+      change.toggleMark(type)
+      return true
+    }
+  }
+)
+
+const plugins = [
+  MarkHotkey({ key: 'b', type: 'bold' }),
+  MarkHotkey({ key: 'ü', type: 'code' }),
+  MarkHotkey({ key: 'i', type: 'italic' }),
+  MarkHotkey({ key: '~', type: 'strikethrough' }),
+  MarkHotkey({ key: 'u', type: 'underline' }),
+];
